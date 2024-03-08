78af350e9bbcf904abded70aeafeabe1c2de6ebf
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 03:41:14 -0500

    Update sonarAnalysis.groovy check v4

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
     echo "${config.valor}"
     echo "${config.branch}"
-    if (${config.valor} == true){
+    if ("${config.valor}" == true){
         echo "Failing the pipeline due to boolean value: ${config.valor}"
         currentBuild.result = 'FAILURE'
         error("Quality Gate failed. Pipeline stopped.")
