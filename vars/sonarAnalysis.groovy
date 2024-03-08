39df6fd3c8fbdb1586f9f8e7d29fb375e621f35a
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 02:44:27 -0500

    Update sonarAnalysis.groovy check v3

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,9 +1,5 @@
 def call(String branchesToAnalyze) {
-    
-    #def shouldFailAlways = System.getenv("FAIL_ALWAYS") ?: false
-    #def shouldFailBasedOnBranch = !shouldFailAlways
-
-    if (shouldFailAlways || gitBranch == 'master' || gitBranch.startsWith('hotfix')) {
+    if (gitBranch == 'master' || gitBranch.startsWith('hotfix')) {
         echo "Failing the pipeline due to branch name: $gitBranch"
         currentBuild.result = 'FAILURE'
         error("Quality Gate failed. Pipeline stopped.")
