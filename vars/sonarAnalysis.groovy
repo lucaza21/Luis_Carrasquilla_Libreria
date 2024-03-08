db10433b5c8ac410e9ac4637fbdb88de4fbcdf1e
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 02:47:16 -0500

    Update sonarAnalysis.groovy check v3

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(String branchesToAnalyze) {
     sh " echo ${branchesToAnalyze} "
-    if ("${branchesToAnalyze}" == 'master' || ${branchesToAnalyze}".startsWith('hotfix')) {
+    if ("${branchesToAnalyze}" == 'master' || "${branchesToAnalyze}".startsWith('hotfix')) {
         echo "Failing the pipeline due to branch name: $gitBranch"
         currentBuild.result = 'FAILURE'
         error("Quality Gate failed. Pipeline stopped.")
