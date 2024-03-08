180dc1bfc6b38affb64e115ea21c14770114fc9b
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 01:01:57 -0500

    Update sonarAnalysis.groovy qualitygate v4

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,8 +1,7 @@
 def call(boolean valor) {
     timeout(time:5, unit:'MINUTES'){
         sh 'echo Iniciando las quality gates'
-        sh "sleep 3"
-        sh "echo ${valor}"
+        sh "echo el parametro enviado es: ${valor}"
         waitForQualityGate abortPipeline: "${valor}"
     }
 }
