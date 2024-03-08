e4c9cf5eed9437c7c8015134e7e2b3806cff0633
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 02:25:30 -0500

    Update sonarAnalysis.groovy check v1

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,9 +1,21 @@
 def call(boolean valor) {
-    stage('Checkout'){
-        checkoutCode()
+    def shouldFailAlways = System.getenv("FAIL_ALWAYS") ?: false
+    def shouldFailBasedOnBranch = !shouldFailAlways
+
+    if (shouldFailBasedOnBranch) {
+        def gitBranch = System.getenv("BRANCH_NAME") ?: 'master'
+    
+        if (shouldFailAlways || gitBranch == 'master' || gitBranch.startsWith('hotfix')) {
+            echo "Failing the pipeline due to branch name: $gitBranch"
+            currentBuild.result = 'FAILURE'
+            error("Quality Gate failed. Pipeline stopped.")
+        } else {
+            echo "Not failing the pipeline for branch: $gitBranch"
+        }
+    } else {
+        echo "Failing the pipeline as per configuration"
+        currentBuild.result = 'FAILURE'
+        error("Quality Gate failed. Pipeline stopped.")
     }
 }
 
-def checkoutCode(){
-    checkout scm
-}
