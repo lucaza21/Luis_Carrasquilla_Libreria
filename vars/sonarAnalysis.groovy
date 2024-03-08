a5c68868f442077e81f4dde747c3d24f7dd4dd97
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 02:17:17 -0500

    Update sonarAnalysis.groovy check v1

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,7 +1,9 @@
 def call(boolean valor) {
-    timeout(time:5, unit:'MINUTES'){
-        sh 'echo Iniciando las quality gates'
-        sh "echo el parametro enviado es: ${valor}"
-        waitForQualityGate abortPipeline: "${valor}"
+    stage('Checkout'){
+        checkoutCode()
     }
+}
+
+def checkoutCode(){
+    checkout scm
 }
