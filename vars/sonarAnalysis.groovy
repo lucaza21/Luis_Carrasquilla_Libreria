d5f4599b21794d28106d84185a1d531e42a134a5
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 03:28:30 -0500

    Update sonarAnalysis.groovy check v4

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,4 +1,6 @@
 def call(Map config = [:]) {
+    echo "${config.valor}"
+    echo "${config.branch}"
     if ("${config.valor}" == true && "${config.branch}" == null){
         echo "Failing the pipeline due to boolean value: ${config.valor}"
         currentBuild.result = 'FAILURE'
