023fb6928f316f35039c68ecd3982950f31424d4
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 00:12:04 -0500

    Update sonarAnalysis.groovy qualitygate v4

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,7 +1,4 @@
 def call(boolean valor) {
-    def SonarEnv(){
-            sh 'echo Ejecucion de las pruebas de calidad de codigo'
-        }
     timeout(time:5, unit:'MINUTES'){
         sh 'echo Iniciando las quality gates'
         sh "sleep 3"
