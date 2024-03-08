1a4ddce8e8d7556a812fc04e11cce14774f2442d
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 02:33:24 -0500

    Update sonarAnalysis.groovy check v1

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,21 +1,22 @@
-def call(boolean valor) {
+def call(List<String> branchesToAnalyze = ['master']) {
     def shouldFailAlways = System.getenv("FAIL_ALWAYS") ?: false
+
     def shouldFailBasedOnBranch = !shouldFailAlways
 
-    if (shouldFailBasedOnBranch) {
-        def gitBranch = System.getenv("BRANCH_NAME") ?: 'master'
-    
-        if (shouldFailAlways || gitBranch == 'master' || gitBranch.startsWith('hotfix')) {
-            echo "Failing the pipeline due to branch name: $gitBranch"
+    branchesToAnalyze.each { gitBranch ->
+        if (shouldFailBasedOnBranch) {
+            if (shouldFailAlways || gitBranch == 'master' || gitBranch.startsWith('hotfix')) {
+                echo "Failing the pipeline due to branch name: $gitBranch"
+                currentBuild.result = 'FAILURE'
+                error("Quality Gate failed. Pipeline stopped.")
+            } else {
+                echo "Not failing the pipeline for branch: $gitBranch"
+            }
+        } else {
+            echo "Failing the pipeline as per configuration"
             currentBuild.result = 'FAILURE'
             error("Quality Gate failed. Pipeline stopped.")
-        } else {
-            echo "Not failing the pipeline for branch: $gitBranch"
         }
-    } else {
-        echo "Failing the pipeline as per configuration"
-        currentBuild.result = 'FAILURE'
-        error("Quality Gate failed. Pipeline stopped.")
     }
 }
 
