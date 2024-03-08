bac1a1fa7c496024ad58e83ef94c3c30030b2b46
lucaza21 <87344617+lucaza21@users.noreply.github.com>
2024-03-08 03:17:20 -0500

    Update sonarAnalysis.groovy check v4

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,11 +1,15 @@
-def call(String branchesToAnalyze) {
-    sh " echo ejecutando la rama: ${branchesToAnalyze} "
-    if ("${branchesToAnalyze}" == 'master' || "${branchesToAnalyze}".startsWith('hotfix')) {
-        echo "Failing the pipeline due to branch name: ${branchesToAnalyze}"
+def call(Boolean valor, String branch) {
+    if (valor == true){
+        echo "Failing the pipeline due to boolean value: ${valor}"
         currentBuild.result = 'FAILURE'
         error("Quality Gate failed. Pipeline stopped.")
-    } else {
-        echo "Not failing the pipeline for branch: ${branchesToAnalyze}"
+    } else if ("${branch}" == 'master' || "${branch}".startsWith('hotfix')) {
+        echo "Failing the pipeline due to branch name: ${branch}"
+        currentBuild.result = 'FAILURE'
+        error("Quality Gate failed. Pipeline stopped.")
+    }
+     else {
+        echo "Not failing the pipeline for branch: ${branch}"
     }  
 }
 
